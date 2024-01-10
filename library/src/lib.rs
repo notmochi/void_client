163a62a26129f1efc8f73abcad428cc615b6b3ec
Silenciado <137856401+silenciado@users.noreply.github.com>
2024-01-10 07:12:57 -0800

    Run Bat & Basic Library

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
+    match call_reason {
+        DLL_PROCESS_ATTACH => (),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
     }
+    true
 }
